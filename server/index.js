a68be06120803048c2f71aa18f815998e82533da
Arpit Deshmukh <arpitdeshmukh21@gmail.com>
2026-03-29 20:04:08 +0530

    feat L: db setup

--- server/index.js
+++ server/index.js
@@ -1,5 +1,11 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -10,6 +16,8 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(` Server running on port ${PORT}`);
 });
