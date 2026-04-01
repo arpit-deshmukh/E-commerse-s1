9f57ca7c262c712171c8b2c8c5cbe75043e2d7e6
Jagdish Uikey <juikey697@gmail.com>
2026-04-01 22:13:45 +0530

    addded the navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 import ProductList from './pages/ProductList.jsx';
 import Cart from './pages/Cart';
-import Header from './components/Header';
+import Navbar from './components/Navbar';
 
 function App() {
   const [page, setPage] = useState('products');
 
   return (
-    <div>
-      <Header setPage={setPage} />
+    <div className="min-h-screen bg-gray-950 text-white">
+      <Navbar setPage={setPage} />
 
-      {page === 'products' && <ProductList />}
-      {page === 'cart' && <Cart />}
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        {page === 'products' && <ProductList />}
+        {page === 'cart' && <Cart />}
+      </main>
     </div>
   );
 }
