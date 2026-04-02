b36ee636ed287c168efd6f490dc2752f28592aca
Arpit Deshmukh <arpitdeshmukh21@gmail.com>
2026-04-02 19:35:21 +0530

    fix : tailwind installation issue resolved

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,14 +4,4 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    port: 5173
-  }
 })
-
-
-// server: {
-//   proxy: {
-//     "/api": "http://localhost:5000"
-//   }
-// }
